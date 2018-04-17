9697b9d1bd4aed7c94fe41071d99063da49d5257
Shiau Hui <shiauhui@172-15-0-27.lightspeed.stlsmo.sbcglobal.net>
2018-04-17 21:38:58 +0800

    test

---
+++ test.php
@@ -0,0 +1,51 @@
+<?php
+require_once '/vendor/autoload.php';
+
+// Create the Transport
+$transport = (new Swift_SmtpTransport('in-v3.mailjet.com', 587))
+  ->setUsername('6d38990fb93bd51ad01237a14c82b892')
+  ->setPassword('609c7ab845b1005f0af01b3b01d65d82')
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message('Wonderful Subject'))
+  ->setFrom(['lshui5122@gmail.com' => 'ShiauHui'])
+  ->setTo(['lshui5122@yahoo.com', 'lshui5122@yahoo.com' => 'TestShiauHui'])
+  ->setBody('Here is the message itself')
+  ;
+
+// Send the message
+$result = $mailer->send($message);
+?>
+
+<div class="container-fluid " style="height:100%; background-color: #eaf4f2; padding:30px;"> 
+
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-3"></div>
+
+    <div class="col-lg-4 col-md-4 col-sm-6">
+        <h1 class="text-center" style="color:#58ba52;"><strong>Reset Password</strong></h1><br>
+       
+        <p class="text-center" style="color:red;"> <?php echo $formerror?></p>
+        
+        <p class="text-center" style="color:#727272">Please enter your email</p>
+  
+          <form name="myForm" method="post" class="text-center">
+              <div class="form-group">
+          <input type="email" name="email" placeholder="Email" value="<?=$email?>" pattern=".{1,}" required title="Cannot be empty field" size="30"><?php echo $error_name?>
+              </div>
+      <br>
+  
+  <button type="submit" name="submitted" value="Submit" class="btn btn-primary" style="height:50px; width:80%;background-color: #2f8f83;
+              color:white; border: 2px solid white;border-radius: 25px;">Reset Password</button>
+
+          </form>
+        
+      </div>
+
+    <div class="col-lg-4 col-md-4 col-sm-3"></div>
+</div>
+</div>
